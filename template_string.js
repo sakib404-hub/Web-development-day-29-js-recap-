6c115de009d1abdf775752796bb6217db887bccb
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-05 02:11:56 +0600

    backtick that is known as the backtick

---
+++ template_string.js
@@ -0,0 +1,19 @@
+const poem = "Am pata Jora Jora";
+
+// template for writing things in different lines
+
+const kobita = `Am pata Jora Jora
+                Marbo Chabuk Chorbo ghora
+                Ore BUBU Sore Dara
+                Asce Amar Pagla Ghora`;
+console.log(kobita);
+
+// const num1 = 10;
+// const num2 = 20;
+
+function addition(numberOne = 0, numberTwo = 0) {
+  const total = numberOne + numberTwo;
+  console.log(`Sum of ${numberOne} and ${numberTwo} is ${total}`);
+}
+
+addition(40, 50);
