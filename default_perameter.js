b2d678c8203ec580c0a2b19fb19580e5373fb772
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-05 02:01:36 +0600

    default values

--- default_perameter.js
+++ default_perameter.js
@@ -1,8 +1,31 @@
-function addition(numberOne, numberTwo) {
+function addition(numberOne = 0, numberTwo = 0) {
   //value that are decleared in the function as the value is getting while
   //calling these are known as the perameter
-  return numberOne + numberTwo;
+  //   const undefined = number + undefined
+  const sum = numberOne + numberTwo;
+  console.log(numberOne, numberTwo, sum);
 }
 
-console.log(addition(10, 6));
+addition(10);
 //When we are calling the function with those perameters those are known as the argumnets
+
+function multiply(numberOne = 1, numberTwo = 1) {
+  let multiply = numberOne * numberTwo;
+  console.log(multiply);
+}
+
+multiply(10, 10);
+multiply(10);
+
+function fullName(firstName = "", lastName = "") {
+  let fullname = firstName + " " + lastName;
+  console.log(fullName);
+}
+
+/**
+ * When we are doing the addition the default value will be ----> 0
+ * when we are doing the substraction the default valus will be -----> 0
+ * when we are doing the multiplication the default value will be ---->1
+ * when we are operating with some array the default value will be black array----->[]
+ * when are dealing with the string it can be empty string -----> " "
+ */
